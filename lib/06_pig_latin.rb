417b343b04316fc865edfed218f06fc1be3d94d0
Florian-THP <triboutflorian@outlook.com>
2024-10-07 16:22:07 +0200

    Ajout de quelque commentaire

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -3,15 +3,16 @@ def translate(string)
   voyelles = ["a", "e", "i", "o", "u"]
   
   string.split.each do |mot|
-    if mot.start_with?("sch")
-      debut = mot.slice!(0, 3)
-      resultat << mot.concat(debut) + "ay"
+    if mot.start_with?("sch") # regarde si le mot commence par sch
+      debut = mot.slice!(0, 3) # enleve du mot tableau les 3 premiere caractere
+      resultat << mot.concat(debut) + "ay" # ajoute les 3premiere caracter a la fin + "ay"
       
     elsif mot.start_with?("qu")
       debut = mot.slice!(0, 2)
       resultat << mot.concat(debut) + "ay"
 
-    elsif !voyelles.include?(mot[0].downcase) && mot[1] == "q" && mot[2] == "u"
+    elsif !voyelles.include?(mot[0].downcase) && mot[1] == "q" && mot[2] == "u" 
+      #verifie si le mot commence par une consone et si la 2 lettre du mot et "q" et si la 3 eme lettre du mot et "u"
       debut = mot.slice!(0, 3)
       resultat << mot.concat(debut) + "ay"
     elsif voyelles.include?(mot[0].downcase)
